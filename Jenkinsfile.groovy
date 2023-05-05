77b948ad4b083ea08b4d61a2d3a1cd23449670b0
balu <balumahendranss@gmail.com>
2023-05-06 00:24:16 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -87,52 +87,7 @@ pipeline {
          }
         }
      }
-      stage('Create EKS Cluster : Terraform'){
-            steps{
-                script{
-
-                    dir('eks_module') {
-                      sh """
-                          
-                          terraform init 
-                          terraform plan -var 'access_key=$ACCESS_KEY' -var 'secret_key=$SECRET_KEY' -var 'region=$region' --var-file=./config/terraform.tfvars
-                          terraform apply -var 'access_key=$ACCESS_KEY' -var 'secret_key=$SECRET_KEY' -var 'region=$region' --var-file=./config/terraform.tfvars --auto-approve
-                      """
-                  }
-                }
-            }
-        }
-     stage('Connect to EKS '){
-           steps{
-              script{
-             sh  'aws eks --region "$region" update-kubeconfig --name "$cluster_name" '
-             
-            }
-        }
-        } 
-        stage('Deployment on EKS Cluster'){
-           steps{
-                script{
-                  
-                  def apply = false
-
-                  try{
-                    input message: 'please confirm to deploy on eks', ok: 'Ready to apply the config ?'
-                    apply = true
-                  }catch(err){
-                    apply= false
-                    currentBuild.result  = 'UNSTABLE'
-                  }
-                  if(apply){
-
-                    sh """
-                      kubectl apply -f deployment.yaml
-                      kubectl apply -f service.yaml
-                    """
-                  }
-                }
-            }
-        }    
+      
     }
 }
 
